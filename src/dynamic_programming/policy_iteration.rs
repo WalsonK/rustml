f57b43f7e6c3c621c98002487044c355ed555c5c
WalsonK <71435596+WalsonK@users.noreply.github.com>
2024-05-29 22:32:05 +0200

    Fix policy eval

--- src/dynamic_programming/policy_iteration.rs
+++ src/dynamic_programming/policy_iteration.rs
@@ -3,7 +3,8 @@ pub struct PolicyEvaluationModel {
     pub num_states: usize,
     pub num_actions: usize,
     pub rewards: usize,
-    pub probabilities: Vec<Vec<Vec<f32>>>
+    pub probabilities: Vec<Vec<Vec<f32>>>,
+    pub is_policy_stable: bool
 }
 
 impl PolicyEvaluationModel {
@@ -13,41 +14,45 @@ impl PolicyEvaluationModel {
             num_actions: a,
             rewards: r,
             probabilities: p, // vec![vec![vec![vec![0.0; 2]; 2]; 2]; 2]
+            is_policy_stable: false
         });
 
         pe_model
     }
 
-    fn policy_evaluation(&self, theta: f32) {
+    fn policy_evaluation(&self, theta: f32) -> Vec<f32> {
         let gamma: f32 = 0.999;
-        let mut policy = vec![0, self.num_states];
-        let mut policy_stable = false;
+        let mut v = vec![0.0; self.num_states];
 
-        while !policy_stable {
-            let mut v = vec![0.0, self.num_states as f32];
-            let mut delta;
 
-            loop {
-                delta = 0.0;
-                for state_index in 0..self.num_states {
-                    let old_value = v[state_index];
-                    let mut new_val = 0.0;
-                    for action_index in 0..self.num_actions {
-                        let mut action_val = 0.0;
-                        for next_state_index in 0..self.num_states {
-                            action_val += self.probabilities[state_index][action_index][next_state_index]
-                                * (self.rewards[state_index][action_index][next_state_index]
-                                + gamma * v[next_state_index]);
-                        }
-                        new_val = new_val.max(action_val);
+        loop {
+            let mut delta = 0.0;
+
+            for state_index in 0..self.num_states {
+                let old_value = v[state_index];
+                let mut action_val = 0.0;
+
+                for action_index in 0..self.num_actions {
+                    for next_state_index in 0..self.num_states {
+                        action_val += self.probabilities[state_index][action_index][next_state_index]
+                            * (self.rewards[state_index][action_index][next_state_index]
+                            + gamma * v[next_state_index]);
                     }
-                    v[state_index] = new_val;
-                    delta = delta.max((old_value - v[state_index]).abs());
                 }
-                if delta < theta {
-                    break;
-                }
+                v[state_index] = action_val;
+                delta = delta.max((old_value - v[state_index]).abs());
             }
+
+            if delta < theta { break; }
+        }
+        v
+    }
+
+    fn policy_improvement(&mut self, policy: Vec<i32>) {
+        self.is_policy_stable = true;
+        for s in 0..self.num_states {
+            let old_action = policy[s];
+
         }
     }
 }
