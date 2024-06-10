f22e9919537511cc902424ac3187df42d3887017
WalsonK <71435596+WalsonK@users.noreply.github.com>
2024-06-10 22:09:38 +0200

    ✨ Policy iteration

--- src/dynamic_programming/policy_iteration.rs
+++ src/dynamic_programming/policy_iteration.rs
@@ -5,7 +5,8 @@ pub struct PolicyEvaluationModel {
     pub rewards: usize,
     pub probabilities: Vec<Vec<Vec<f32>>>,
     pub gamma: f32,
-    pub is_policy_stable: bool
+    pub is_policy_stable: bool,
+    pub policy: Vec<f32>
 }
 
 impl PolicyEvaluationModel {
@@ -16,44 +17,43 @@ impl PolicyEvaluationModel {
             rewards: r,
             probabilities: p, // vec![vec![vec![vec![0.0; 2]; 2]; 2]; 2]
             is_policy_stable: false,
-            gamma: g
+            gamma: g,
+            policy: vec![0.0; s]
         });
 
         pe_model
     }
 
-    fn policy_evaluation(&self, theta: f32) -> Vec<f32> {
+    fn policy_evaluation(&self, theta: f32) {
         // let gamma: f32 = 0.999;
-        let mut v = vec![0.0; self.num_states];
 
 
         loop {
             let mut delta = 0.0;
 
             for state_index in 0..self.num_states {
-                let old_value = v[state_index];
+                let old_value = self.policy[state_index];
                 let mut action_val = 0.0;
 
                 for action_index in 0..self.num_actions {
                     for next_state_index in 0..self.num_states {
                         action_val += self.probabilities[state_index][action_index][next_state_index]
                             * (self.rewards[state_index][action_index][next_state_index]
-                            + self.gamma * v[next_state_index]);
+                            + self.gamma * self.policy[next_state_index]);
                     }
                 }
-                v[state_index] = action_val;
-                delta = delta.max((old_value - v[state_index]).abs());
+                self.policy[state_index] = action_val;
+                delta = delta.max((old_value - self.policy[state_index]).abs());
             }
 
             if delta < theta { break; }
         }
-        v
     }
 
-    fn policy_improvement(&mut self, policy: Vec<i32>) {
+    fn policy_improvement(&mut self) -> bool{
         self.is_policy_stable = true;
         for state_index in 0..self.num_states {
-            let old_action = policy[state_index];
+            let old_action = self.policy[state_index];
             let mut best_action: usize = 0;
             let mut best_action_score = -99999.9;
 
@@ -63,17 +63,26 @@ impl PolicyEvaluationModel {
                 for next_state_index in 0..self.num_states {
                     total += self.probabilities[state_index][action_index][next_state_index] *
                         (self.rewards[state_index][action_index][next_state_index]
-                        + self.gamma * policy[next_state_index] as f32)
+                        + self.gamma * self.policy[next_state_index] as f32)
                 }
                 if best_action == 0 || total >= best_action_score {
                     best_action = action_index;
                     best_action_score = total;
                 }
             }
-            policy[state_index] = best_action as i32;
-            if policy[state_index] != old_action {
+            self.policy[state_index] = best_action as f32;
+            if self.policy[state_index] != old_action {
                 self.is_policy_stable = false;
             }
         }
+        return self.is_policy_stable
+    }
+
+    fn policy_iteration(&mut self) -> &Vec<f32>{
+        loop {
+            self.policy_evaluation(0.000001);
+            if self.policy_improvement() { break; }
+        }
+        return &self.policy
     }
 }
